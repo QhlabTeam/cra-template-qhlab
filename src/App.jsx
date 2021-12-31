b26cfc7c54407b256402484214da711d19624be0
Yoki Yu <yokiyuqian@gmail.com>
2021-12-31 15:47:54 +0800

    chore: update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {Global as GlobalStyle} from '@emotion/react';
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import {HomePage} from './containers/HomePage';
 import {NotFoundPage} from './containers/NotFoundPage';
 import {rebase} from './global.css';
@@ -9,12 +9,10 @@ function App() {
   return (
     <>
       {/* register your routes */}
-      <Router>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route element={<NotFoundPage />} path='*' />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route element={<NotFoundPage />} path='*' />
+      </Routes>
 
       {/* register your global styles */}
       <GlobalStyle styles={[rebase]} />
