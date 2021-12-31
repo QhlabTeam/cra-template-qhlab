d1f5e7dfacbefb4dde5432cc41818cdfaac2706c
Yoki Yu <yokiyuqian@gmail.com>
2021-12-31 14:36:00 +0800

    chore: hooks

---
+++ src/hooks/example.js
@@ -0,0 +1,15 @@
+// write your custom hooks e.g.
+// export function useCount(default) {
+//   const [count, setCount] = useState(default)
+
+//   function increase() {
+//     setCount(count+1)
+//   }
+//   function decrease() {
+//     setCount(count-1)
+//   }
+
+//   return {
+//     count,increase, decrease
+//   }
+// }
