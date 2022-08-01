6e06902f1a40327fe454d00a7fab08dc24b06701
Yoki Yu <yokiyuqian@gmail.com>
2022-08-01 11:27:01 +0800

    feat: add swr and axios, create request.js lib

--- src/AppProviders.jsx
+++ src/AppProviders.jsx
@@ -2,15 +2,19 @@ import {Global as GlobalStyle} from '@emotion/react';
 import {ClickToComponent} from 'click-to-react-component';
 import React from 'react';
 import {unstable_HistoryRouter as HistoryRouter} from 'react-router-dom';
+import {SWRConfig} from 'swr';
 
 import {electronOnly, layout, rebase, themeing} from './lib/global.css';
 import {history} from './lib/history';
+import {request} from './lib/request';
 
 export function AppProviders({children}) {
   return (
     <>
       {/* Providers */}
-      <HistoryRouter history={history}>{children}</HistoryRouter>
+      <SWRConfig value={{fetcher: request.get}}>
+        <HistoryRouter history={history}>{children}</HistoryRouter>
+      </SWRConfig>
 
       {/* option/alt + click ui to open source code  */}
       <ClickToComponent />
