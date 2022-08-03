2e59a37ed1893dcac045820173a7bfb347dca04e
Yoki Yu <74389358+iamyoki@users.noreply.github.com>
2022-08-03 14:42:38 +0800

    fix: reactDOM.render in react18

--- src/index.jsx
+++ src/index.jsx
@@ -15,8 +15,7 @@ const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
