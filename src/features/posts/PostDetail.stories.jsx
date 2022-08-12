caf1d68315ed95a72dbc7ace4faf026755eb6950
Yoki Yu <74389358+iamyoki@users.noreply.github.com>
2022-08-12 16:26:36 +0800

    chore: remove components stories

--- src/features/posts/PostDetail.stories.jsx
+++ src/features/posts/PostDetail.stories.jsx
@@ -1,3 +1,5 @@
+import {Image} from '../../components/Image';
+import {PostDetailContainer} from './components';
 import {PostDetail} from './PostDetail';
 
 /** @type {import('@storybook/react').Meta} */
@@ -9,4 +11,24 @@ export default {
 export const Basic = (args) => <PostDetail id={args.id} />;
 Basic.args = {
   id: 1,
+};
+
+export const Static = (args) => (
+  <PostDetailContainer className='PostDetail'>
+    <h2 style={{textTransform: 'capitalize'}}>{args.title}</h2>
+    <div>
+      <Image size={40} src={args.avatar} />
+      <address>
+        {args.firstName} {args.lastName}
+      </address>
+    </div>
+    <p>{args.body}</p>
+  </PostDetailContainer>
+);
+Static.args = {
+  title: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam?',
+  avatar: 'https://randomuser.me/api/portraits/thumb/men/42.jpg',
+  firstName: 'John',
+  lastName: 'Doe',
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi sint eius vitae qui, eveniet maiores voluptate, suscipit aliquid laboriosam aliquam perspiciatis nisi assumenda. Iusto voluptatum aliquid natus ratione omnis est!',
 };
--- src/features/posts/PostList.stories.jsx
+++ src/features/posts/PostList.stories.jsx
@@ -1,4 +1,12 @@
+import {RiArrowLeftLine, RiArrowRightLine} from 'react-icons/ri';
+
+import {
+  PostListItem,
+  PostListPagination,
+  PostListPaginationButton,
+} from './components';
 import {PostList} from './PostList';
+import {PostListItemAvatar} from './PostListItemAvatar';
 
 /** @type {import('@storybook/react').Meta} */
 export default {
@@ -10,3 +18,40 @@ export default {
   },
 };
 export const Basic = (args) => <PostList />;
+
+export const StaticItem = (args) => (
+  <PostListItem>
+    <PostListItemAvatar seed={args.seed} />
+    <div>
+      <h2 style={{textTransform: 'capitalize'}}>{args.title}</h2>
+      <p>{args.body}</p>
+    </div>
+  </PostListItem>
+);
+StaticItem.args = {
+  /** Avatar random seed */
+  seed: 1,
+  title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quod vel tenetur ipsum harum, ad sequi praesentium expedita facilis eaque consequuntur, cupiditate iusto, veritatis incidunt totam dolor voluptas pariatur. Ratione?',
+};
+StaticItem.argTypes = {
+  seed: {description: 'Avatar random seed'},
+  title: {description: 'Post title'},
+  body: {description: 'Post description'},
+};
+
+export const StaticPagination = ({onClickPrev, onClickNext}) => (
+  <PostListPagination>
+    <PostListPaginationButton disabled onClick={onClickPrev}>
+      <RiArrowLeftLine />
+    </PostListPaginationButton>
+
+    <PostListPaginationButton onClick={onClickNext}>
+      <RiArrowRightLine />
+    </PostListPaginationButton>
+  </PostListPagination>
+);
+StaticPagination.argTypes = {
+  onClickPrev: {action: 'onClickPrev'},
+  onClickNext: {action: 'onClickNext'},
+};
