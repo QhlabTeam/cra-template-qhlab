1ae943b516412ce58290d8045d8c25de13daeed8
Yoki Yu <74389358+iamyoki@users.noreply.github.com>
2022-08-29 14:17:11 +0800

    fix: storybook webpack config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -19,6 +21,32 @@ module.exports = {
     );
 
     babelRule.options.presets.push('@emotion/babel-preset-css-prop');
+
+    // Webpack5 needs manually fallbacks
+    // https://github.com/facebook/create-react-app/issues/11756#issuecomment-1001769356
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer'),
+    };
+
+    config.plugins = [
+      ...config.plugins,
+      // Fix buffer not defined
+      // https://stackoverflow.com/questions/68707553/uncaught-referenceerror-buffer-is-not-defined
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+      }),
+      // Fix tree shaking problem
+      // https://github.com/facebook/create-react-app/issues/9674#issuecomment-1096270248
+      // new webpack.DefinePlugin({
+      //   ...(!process.env.REACT_APP_ENABLE_MSW && {
+      //     'process.env.REACT_APP_ENABLE_MSW': false,
+      //   }),
+      // }),
+    ];
 
     return config;
   },
