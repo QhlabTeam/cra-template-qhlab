241755d05df2c29e5133a3186246a3ab1fe5e306
Yoki Yu <74389358+iamyoki@users.noreply.github.com>
2022-08-30 14:53:16 +0800

    refactor: rename mocks -> mock

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import {setupMocks} from './mocks/setupMocks';
+import {setupMocks} from './mock/setupMocks';
 import reportWebVitals from './reportWebVitals';
 
 // If you want to enable msw mocks, pass an env:
